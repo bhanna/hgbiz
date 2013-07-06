14493c159ed4535690fad898c7dc5f9a7d86561e
Ben Hanna <ben@benhanna.com>
2013-07-06 16:01:24 -0700

    tooltip

--- wp-content/themes/rachelbaker-bootstrapwp-Twitter-Bootstrap-for-WordPress-9bc2021/footer.php
+++ wp-content/themes/rachelbaker-bootstrapwp-Twitter-Bootstrap-for-WordPress-9bc2021/footer.php
@@ -23,6 +23,11 @@
 <?php wp_footer(); ?>
 
   </body>
+  <script type="text/javascript"> 
+  $("a").tooltip({
+        'selector': '',
+        'placement': 'top'
+    });</script>
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
